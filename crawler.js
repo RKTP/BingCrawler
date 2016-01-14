ab7ecefa954da2df777fde88290c4fe8e7614405
jediweon <archetype93@gmail.com>
2016-01-14 16:14:05 +0900

    reindentation + handlebars.js script added

--- crawler.js
+++ crawler.js
@@ -4,8 +4,8 @@
 	var imageRef = ref.child("images");
 
 //required data for query
-	var bingQuery = {
-		queryUrl: "https://api.datamarket.azure.com/Bing/Search/Image",
+var bingQuery = {
+	queryUrl: "https://api.datamarket.azure.com/Bing/Search/Image",
 		encodedKey: btoa(":NgrxKg56U9UaZq0w0z4ZEKRxgMt7LDGjKaHrPgpoY7M="),//One's Azure account key
 		inputText: "",
 		setHeader: function(xhr) {
@@ -17,162 +17,162 @@
 	}
 
 //query
-	function requestQuery(config, callback) {
-		$.ajax({
-			beforeSend: config.setHeader,
-			type: "GET",
-			url: config.queryUrl,
-			data: {
-				$format: config.format,
-				Query: encodeURIComponent("'"+config.inputText+"'"),
-				$top: config.item,
-				$skip: config.page++*(config.item)
-			},
-			dataType: "json",
-			success: function(responseData) {
-				var results = responseData["d"]["results"];
-				callback(results.filter(function(result) {
-					if (resultMap[result["MediaUrl"]]) {
-						console.error("Duplicated:", result["MediaUrl"]);
-						return false;
-					}
-					resultMap[result["MediaUrl"]] = result;
-					return true;
-				}));
+function requestQuery(config, callback) {
+	$.ajax({
+		beforeSend: config.setHeader,
+		type: "GET",
+		url: config.queryUrl,
+		data: {
+			$format: config.format,
+			Query: encodeURIComponent("'"+config.inputText+"'"),
+			$top: config.item,
+			$skip: config.page++*(config.item)
+		},
+		dataType: "json",
+		success: function(responseData) {
+			var results = responseData["d"]["results"];
+			callback(results.filter(function(result) {
+				if (resultMap[result["MediaUrl"]]) {
+					console.error("Duplicated:", result["MediaUrl"]);
+					return false;
+				}
+				resultMap[result["MediaUrl"]] = result;
+				return true;
+			}));
+		}
+	});
+}
+
+//buffer
+function buffer(callback) {
+	var storage = [];
+	var minSize = 1;
+
+	function callBufferedWrappers() {
+		var wrapper;
+		while (storage.length > minSize && storage[0].args) {
+			wrapper = storage.shift();
+			wrapper.apply(this, wrapper.args);
+		}
+	}
+
+	return {
+		wrapper: function(optionalCallback) {
+			var wrapper = function() {
+				(optionalCallback || callback).apply(this, arguments);
+			};
+			storage.push(wrapper);
+			callBufferedWrappers();
+
+			return function() {
+				wrapper.args = arguments;
+				callBufferedWrappers();
+			};
+		},
+		clear: function() {
+			storage = [];
+		}
+	};
+}
+
+//draw
+function appendImage(result) {
+	var parent = document.getElementById("result");
+	var image, check, template, label;
+	for(var i = 0; i < result.length; i++) {
+		template = document.createElement("div");
+		template.className = "imageView";
+		parent.appendChild(template);
+
+		label = document.createElement("label");
+
+		image = document.createElement("img");
+		image.src = result[i]["MediaUrl"];
+		image.className = "image";
+		label.appendChild(image);
+
+		check = document.createElement("input");
+		check.type = "checkbox";
+		check.className = "selection";
+		check.checked = true;
+		label.appendChild(check);
+
+		template.appendChild(label);
+
+		parent.appendChild(document.createElement("br"));
+	}
+}
+
+var BufferedCrawler = function(callback) {
+	var bufferInstance = buffer(callback);
+	return {
+		getFirstPage: function(config, optionalCallback) {
+			bufferInstance.clear();
+			requestQuery(config, optionalCallback || callback);
+			requestQuery(config, bufferInstance.wrapper(optionalCallback));
+		},
+		getNewPage: function(config, optionalCallback) {
+			requestQuery(config, bufferInstance.wrapper(optionalCallback));
+		}
+	};
+};
+
+var UnbufferedCrawler = function(callback) {
+	return {
+		getFirstPage: function(config, optionalCallback) {
+			requestQuery(config, optionalCallback || callback);
+		},
+		getNewPage: function(config, optionalCallback) {
+			requestQuery(config, optionalCallback || callback);
+		}
+	};
+};
+
+//upload to Firebase DB
+function uploadImg() {
+	var label = this.children[0];
+	if($(label.children[1]).is(':checked')) {
+		var jsonData = resultMap[label.children[0].src];
+		var location = new URL(jsonData["MediaUrl"]);
+		var host = btoa(location.origin);
+		var path = btoa(location.href);
+		var iref = imageRef.child(host).child(path);
+		iref.once('value',function(snapshot) {
+			if(!snapshot.exists()) {
+				iref.set(jsonData);
 			}
 		});
 	}
-
-//buffer
-	function buffer(callback) {
-		var storage = [];
-		var minSize = 1;
-
-		function callBufferedWrappers() {
-			var wrapper;
-			while (storage.length > minSize && storage[0].args) {
-				wrapper = storage.shift();
-				wrapper.apply(this, wrapper.args);
-			}
-		}
-
-		return {
-			wrapper: function(optionalCallback) {
-				var wrapper = function() {
-					(optionalCallback || callback).apply(this, arguments);
-				};
-				storage.push(wrapper);
-				callBufferedWrappers();
-
-				return function() {
-					wrapper.args = arguments;
-					callBufferedWrappers();
-				};
-			},
-			clear: function() {
-				storage = [];
-			}
-		};
-	}
-
-//draw
-	function appendImage(result) {
-		var parent = document.getElementById("result");
-		var image, check, template, label;
-		for(var i = 0; i < result.length; i++) {
-			template = document.createElement("div");
-			template.className = "imageView";
-			parent.appendChild(template);
-
-			label = document.createElement("label");
-
-			image = document.createElement("img");
-			image.src = result[i]["MediaUrl"];
-			image.className = "image";
-			label.appendChild(image);
-
-			check = document.createElement("input");
-			check.type = "checkbox";
-			check.className = "selection";
-			check.checked = true;
-			label.appendChild(check);
-
-			template.appendChild(label);
-
-			parent.appendChild(document.createElement("br"));
-		}
-	}
-
-	var BufferedCrawler = function(callback) {
-		var bufferInstance = buffer(callback);
-		return {
-			getFirstPage: function(config, optionalCallback) {
-				bufferInstance.clear();
-				requestQuery(config, optionalCallback || callback);
-				requestQuery(config, bufferInstance.wrapper(optionalCallback));
-			},
-			getNewPage: function(config, optionalCallback) {
-				requestQuery(config, bufferInstance.wrapper(optionalCallback));
-			}
-		};
-	};
-
-	var UnbufferedCrawler = function(callback) {
-		return {
-			getFirstPage: function(config, optionalCallback) {
-				requestQuery(config, optionalCallback || callback);
-			},
-			getNewPage: function(config, optionalCallback) {
-				requestQuery(config, optionalCallback || callback);
-			}
-		};
-	};
-
-//upload to Firebase DB
-	function uploadImg() {
-		var label = this.children[0];
-		if($(label.children[1]).is(':checked')) {
-			var jsonData = resultMap[label.children[0].src];
-			var location = new URL(jsonData["MediaUrl"]);
-			var host = btoa(location.origin);
-			var path = btoa(location.href);
-			var iref = imageRef.child(host).child(path);
-			iref.once('value',function(snapshot) {
-				if(!snapshot.exists()) {
-					iref.set(jsonData);
-				}
-			});
-		}
-	}
+}
 
 //event handling
-	var crawler = BufferedCrawler(appendImage);
+var crawler = BufferedCrawler(appendImage);
 
-	$("#keyword").keyup(function(event) {
-		if(event.keyCode == 13) {
-			$("#btn_search").click();
-		}
-	});
+$("#keyword").keyup(function(event) {
+	if(event.keyCode == 13) {
+		$("#btn_search").click();
+	}
+});
 
-	$('#btn_save').click(function() {
-		$('#result').find(".imageView").map(uploadImg);
-	});
+$('#btn_save').click(function() {
+	$('#result').find(".imageView").map(uploadImg);
+});
 
-	$('#btn_search').click(function() {
-		resultMap = {};
+$('#btn_search').click(function() {
+	resultMap = {};
+	var targetApi = bingQuery;
+	targetApi.inputText = $('#keyword').val();
+	$('#result').empty();
+	$('#label').text(targetApi.inputText);
+	
+	targetApi.page = 0;
+	crawler.getFirstPage(targetApi);
+});
+
+$(document).endlessScroll({
+	callback:function(n) {
 		var targetApi = bingQuery;
-		targetApi.inputText = $('#keyword').val();
-		$('#result').empty();
-		$('#label').text(targetApi.inputText);
-		
-		targetApi.page = 0;
-		crawler.getFirstPage(targetApi);
-	});
-
-	$(document).endlessScroll({
-		callback:function(n) {
-			var targetApi = bingQuery;
-			crawler.getNewPage(targetApi);
-		}
-	});
+		crawler.getNewPage(targetApi);
+	}
+});
 })(jQuery);
